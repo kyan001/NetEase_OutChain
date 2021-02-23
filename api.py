f8f7d7695f7c76b93f231677b63b49bca760fdf2
Kyan <ykyankai@gmail.com>
2021-02-23 12:14:17 +0800

    ask for input when no argv provided
    Aligned code style to pep8

--- api.py
+++ api.py
@@ -1,25 +1,30 @@
 import os
-import re
 import json
-import hashlib
 import base64
 import binascii
-import logging  
-import logging.handlers  
 import sys
 
 from Crypto.Cipher import AES
 import requests
 
-default_timeout = 10
+
+MODULUS = ('00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7'
+           'b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280'
+           '104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932'
+           '575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b'
+           '3ece0462db0a22b8e7')
+NONCE = '0CoJUm6Qyw8W8jud'
+PUBKEY = '010001'
+
 
 def encrypted_request(text):
     text = json.dumps(text)
     secKey = createSecretKey(16)
-    encText = aesEncrypt(aesEncrypt(text, nonce), secKey)
-    encSecKey = rsaEncrypt(secKey, pubKey, modulus)
+    encText = aesEncrypt(aesEncrypt(text, NONCE), secKey)
+    encSecKey = rsaEncrypt(secKey, PUBKEY, MODULUS)
     data = {'params': encText, 'encSecKey': encSecKey}
     return data
+
 
 def aesEncrypt(text, secKey):
     pad = 16 - len(text) % 16
@@ -39,16 +44,8 @@ def rsaEncrypt(text, pubKey, modulus):
 def createSecretKey(size):
     return binascii.hexlify(os.urandom(size))[:16]
 
-modulus = ('00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7'
-           'b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280'
-           '104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932'
-           '575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b'
-           '3ece0462db0a22b8e7')
-nonce = '0CoJUm6Qyw8W8jud'
-pubKey = '010001'
 
 class NetEase(object):
-
     def __init__(self):
         self.header = {
             'Accept': '*/*',
@@ -61,19 +58,35 @@ class NetEase(object):
             'User-Agent':
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36'  # NOQA
         }
-        self.cookies = {'appver': '1.5.2','os': 'linux'}
+        self.cookies = {
+            'appver': '1.5.2',
+            'os': 'linux'
+        }
         self.session = requests.Session()
 
     def songs_detail_new_api(self, music_ids, bit_rate=320000):
         action = 'http://music.163.com/weapi/song/enhance/player/url?csrf_token='
-        data = {'ids': music_ids, 'br': bit_rate, 'csrf_token': ''}
-        connection = self.session.post(action,
-                                       data=encrypted_request(data),
-                                       cookies=self.cookies,
-                                       headers=self.header)
+        data = {
+            'ids': music_ids,
+            'br': bit_rate,
+            'csrf_token': ''
+        }
+        connection = self.session.post(action, data=encrypted_request(data), cookies=self.cookies, headers=self.header)
         return connection.text
 
-details = NetEase().songs_detail_new_api(sys.argv[1].split(','))
-#print(details)
-details = json.loads(details)
-print(details['data'][0]['url'])
+
+def main():
+    if len(sys.argv) <= 1:
+        song_id = input("Enter Song ID: ")
+    else:
+        song_id = sys.argv[1]
+    details = NetEase().songs_detail_new_api(song_id.split(','))
+    details = json.loads(details)
+    song_data = details['data'][0]
+    print("Size: {:.2f} MB".format(song_data['size'] / 1024 / 1024))
+    print("URL:", song_data['url'])
+    return song_data['url']
+
+
+if __name__ == '__main__':
+    main()
